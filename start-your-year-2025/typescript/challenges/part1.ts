93db5b393a0e3d51bb24cbb978eb03168f307ca8
mobiuschair <mobiuschair001@gmail.com>
2025-01-17 07:33:48 +0000

    working code

--- start-your-year-2025/typescript/challenges/part1.ts
+++
@@ -1,9 +0,0 @@
-function plus(x: string | undefined | number, y: string | number) {
-    return (typeof x === "number" && typeof y === "number") ? x + y :
-           (x === undefined) ? undefined :
-           (typeof x === "string" && typeof y === "string") ? x + y : undefined;
-}
-
-console.log(plus(2025, 1))       // 2026
-console.log(plus(undefined, 21));   // undefined
-console.log(plus("StackUp", "Learn"))// StackUpLearn
